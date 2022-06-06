89afd75ce69d2385f915983f8b4015e8390d2b51
Jeffry Gonzalez <jeff@hypertheory.com>
2022-06-06 12:15:16 -0400

    before lunch monday

---
+++ DemoApiSolution/DemoApi/Controllers/StatusController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApi.Controllers;
+
+public class StatusController : ControllerBase
+{
+    // GET /status
+    [HttpGet("/status")]
+    public async Task<ActionResult<StatusResponse>> GetStatus()
+    {
+        var response = new StatusResponse { CreatedAt = DateTime.Now, Message = "Awesome. Party on Wayne" };
+        return Ok(response); // reponse with 200 Ok status code.
+    }
+}
+
+
+public class StatusResponse
+{
+    public string Message { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+
+}
