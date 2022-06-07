c0761e66287ed96f976c464ca73de8bf5f112aed
Jeffry Gonzalez <jeff@hypertheory.com>
2022-06-07 14:52:15 -0400

    started the employee api

---
+++ EmployeesApiSolution/EmployeesApi/Controllers/EmployeesController.cs
@@ -0,0 +1,22 @@
+﻿namespace EmployeesApi.Controllers;
+
+public class EmployeesController : ControllerBase
+{
+
+    [HttpGet("/employees/{id}")]
+    public async Task<ActionResult<EmployeeDocumentResponse>> GetEmployeeByIdAsync(string id)
+    {
+        if(int.Parse(id) > 100)
+        {
+            return NotFound();
+        }
+        var response = new EmployeeDocumentResponse
+        {
+            Id = id,
+            Name = new EmployeeNameInformation { FirstName = "Bob", LastName = "Smith" },
+            Department = "DEV"
+
+        };
+        return Ok(response);
+    }
+}
---
+++ EmployeesApiSolution/EmployeesApi/Models/EmployeeModels.cs
@@ -0,0 +1,32 @@
+﻿namespace EmployeesApi.Models;
+
+public record EmployeeDocumentResponse
+{
+    public string Id
+    {
+        get; set;
+    } = string.Empty;
+
+    public EmployeeNameInformation Name
+    {
+        get; set;
+    } = new();
+
+    public string Department
+    {
+        get; init;
+    } = string.Empty;
+}
+
+public record EmployeeNameInformation
+{
+    public string FirstName
+    {
+        get; init;
+    } = string.Empty;
+
+    public string LastName
+    {
+        get; init;
+    } = string.Empty;
+}
