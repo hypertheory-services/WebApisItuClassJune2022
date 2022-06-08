6cdce193538b9ac49dce6bda5bdcf595c92e8373
Jeffry Gonzalez <jeff@hypertheory.com>
2022-06-08 12:17:17 -0400

    added validation

--- EmployeesApiSolution/EmployeesApi/Controllers/EmployeesController.cs
+++ EmployeesApiSolution/EmployeesApi/Controllers/EmployeesController.cs
@@ -1,5 +1,6 @@
 ﻿namespace EmployeesApi.Controllers;
 
+[ApiController]
 public class EmployeesController : ControllerBase
 {
     private readonly ILookupEmployees _employeeLookup;
@@ -7,6 +8,25 @@ public class EmployeesController : ControllerBase
     public EmployeesController(ILookupEmployees employeeLookup)
     {
         _employeeLookup = employeeLookup;
+    }
+
+    [HttpPost("/employees")]
+    public async Task<ActionResult<EmployeeDocumentResponse>> HireEmployeeAsync([FromBody] EmployeeCreateRequest request)
+    {
+        // 1. Validate it. 
+        //  -- some of it can be declarative (Validation Attributes, FluentValidition)
+        //  -- some of it might be programmatic.
+        //  -- If it is not valid, the CLIENT screwed up, so you send them a 400 - 499
+        //    -- a 400 with and without details.
+
+        // 2. Ask a service to save it or whatever.
+        // 3. Response (default)
+        //   -- Return a 201 Status Code
+        //   -- Consider putting a Location header on the response with the URL of the new resource.
+        //                 Location: http://localhost:1339/employees/398398398938
+        //   -- Be nice and send them a copy of that resource they would get if they followed that location url.
+        //   -- Add a cache header to the response, and that cache header will be for the Location
+        return Ok(request);
     }
 
     [HttpGet("/employees")]
--- EmployeesApiSolution/EmployeesApi/Models/EmployeeModels.cs
+++ EmployeesApiSolution/EmployeesApi/Models/EmployeeModels.cs
@@ -1,4 +1,6 @@
-﻿namespace EmployeesApi.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeesApi.Models;
 
 public record CollectionResponse<T>
 {
@@ -48,4 +50,48 @@ public record EmployeeNameInformation
     {
         get; init;
     } = string.Empty;
+}
+
+
+
+public class EmployeeCreateRequest : IValidatableObject
+{
+    [Required]
+    [MaxLength(255)]
+    [MinLength(1)]
+    public string FirstName
+    {
+        get; set;
+    } = string.Empty;
+    [Required]
+    public string LastName
+    {
+        get; set;
+    } = string.Empty;
+
+    [Required]
+    public string Department
+    {
+        get; set;
+    } = string.Empty;
+
+    [Required]
+    public decimal? StartingSalary
+    {
+        get; set;
+    }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (FirstName.Trim().ToUpper() == "DARTH" && LastName.Trim().ToUpper() == "VADER")
+        {
+            yield return new ValidationResult("We have a strict no Sith policy");
+        }
+
+        if (Department.Trim().ToUpper() == "DEV" && StartingSalary < 300000)
+        {
+            yield return new ValidationResult("Remember who writes your code.",
+                new string[] { nameof(Department), nameof(StartingSalary) });
+        }
+    }
 }
